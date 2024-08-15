ad96c006e721b11a8dfc961ce8fa0a787361c6d1
Manikanta <manikantasidaga@gmail.com>
2024-08-15 12:31:34 +0530

    First commit before build

---
+++ src/main/java/com/capstone/mobilerechargeservice/MobileRechargeServiceApplication.java
@@ -0,0 +1,13 @@
+package com.capstone.mobilerechargeservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MobileRechargeServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MobileRechargeServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/capstone/mobilerechargeservice/MobileRechargeServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capstone.mobilerechargeservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MobileRechargeServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
